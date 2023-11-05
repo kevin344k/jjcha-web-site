0af637c2f3bd25e327a9dc7de1f5ac1e8670d6e0
kevin344k <kevin344k@gmail.com>
2023-11-05 12:31:25 -0500

    se mejora el responsive para tablet y se agrega un lottie en about-me

--- src/public/script/lottieAnimation.js
+++ src/public/script/lottieAnimation.js
@@ -1,6 +1,9 @@
 let animated_live=document.querySelector(".animated_live")
 let ico_live=document.querySelectorAll(".ico-live")
 
+let animation_btc=document.querySelector(".animation-btc")
+
+
 bodymovin.loadAnimation({
     container:animated_live,
     path:'../assets/lottie/live-ondas.json'
@@ -16,4 +19,8 @@ for (let i = 0; i < ico_live.length; i++) {
 }
 
 
+bodymovin.loadAnimation({
+    container:animation_btc,
+    path:'../assets/lottie/only-btc.json'
+})
 
