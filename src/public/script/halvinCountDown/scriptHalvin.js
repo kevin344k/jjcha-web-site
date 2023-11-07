e032954679b2d371e89b134aeeb2efedc07e0976
kevin344k <kevin344k@gmail.com>
2023-11-06 23:47:29 -0500

    halvin countdown  ready

--- src/public/script/halvinCountDown/scriptHalvin.js
+++ src/public/script/halvinCountDown/scriptHalvin.js
@@ -6,14 +6,52 @@ console.log(dateHalvinAprox)
 let diff=dateHalvinAprox.getTime()/1000-dateNow.getTime()/1000
 //in the page-> https://www.bitcoinblockhalf.com/ be setted the second fron now
 let clock
-console.log(dateHalvinAprox)
+console.log(diff)
 
+
+
+const init = async () => {
+        
+    const { bitcoin: { blocks } } = mempoolJS({
+      hostname: 'mempool.space'
+    });
+
+    const blocksTipHeight = await blocks.getBlocksTipHeight();
+
+   let resultHeightBlock= JSON.stringify(blocksTipHeight, undefined, 2);
+   console.log(resultHeightBlock);
+const blockText=document.querySelector('.text-rewards-halvin')
+blockText.textContent= ` Bloque ${resultHeightBlock} de 840.000  ` 
+let CurrentDate=Date.now() //fecha actual
+let timeToHalvin=(840000 - resultHeightBlock)*(9.6*60)
+let dateH=CurrentDate+timeToHalvin*1000
+let dateHalvin=document.querySelector('.text-halvin')
+dateHalvin.textContent= new Date(dateH)
+
+console.log(timeToHalvin);
 $(document).ready(function(){
   
-    clock=$('.clock').FlipClock(diff,{
+    clock=$('.clock').FlipClock(timeToHalvin,{
         clockFace: 'DailyCounter',
         autoStart:true,
         countdown:true
         }
 )})
+
+
+  };
+
+
+
+
+
+
+
+
+  init();
+
+
+
+
+
 
