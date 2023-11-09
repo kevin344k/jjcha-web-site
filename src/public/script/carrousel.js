a167fac1e3bab4e708b4be933504145eb714108b
kevin344k <kevin344k@gmail.com>
2023-11-09 00:41:56 -0500

    almost ready

--- src/public/script/carrousel.js
+++ src/public/script/carrousel.js
@@ -4,3 +4,11 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 2000,
   touch: false
 })
+
+const myModal=document.querySelector("#exampleModal")
+//MODAL CODE
+
+window.addEventListener("load",()=>{
+  myModal.show()
+
+})
